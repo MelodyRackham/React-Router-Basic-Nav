3fed3ff4059fa7e5d3390987ed62d375a6f13dcd
Mel Maki <melody.rackham@gmail.com>
2019-10-14 14:15:04 -0600

    Declared the to as the href on <link> and specified routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
+      <div className='App'>
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link className='links' to='/'>
+            Home
+          </Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to='/components/About'>About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to='/components/Contact'>Contact</Link>
         </div>
       </div>
     </div>
